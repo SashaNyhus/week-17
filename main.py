35bbdde8af6354bfa71bd35756b36d9a30e7e5a6
SashaVM <sashavictoriam@icloud.com>
2021-06-26 15:20:34 -0700

    html templates, start css, check for missing keys

--- main.py
+++ main.py
@@ -14,8 +14,12 @@ def main():
 def postSearchResults():
     songQuery = request.form.get('query')
     result = shazam.search(songQuery, 0)
-    hits = result['tracks']['hits']
-    return render_template('song-results.html', hits=hits, query=songQuery)
+    if 'tracks' in result:
+        hits = result['tracks']['hits']
+        return render_template('song-results.html', hits=hits, query=songQuery)
+    else:
+        message="No results found"
+        return render_template('whoops.html', message=message)
 
 
 @app.route("/recommendations", methods=['POST'])
@@ -24,7 +28,17 @@ def postRecommendations():
     result = shazam.get_recommendations(key)
     if 'tracks' in result:
         tracks = result['tracks']
+        tracks = checkForMissingKeys(tracks)
         return render_template('recommendations.html', tracks=tracks)
     else:
         message="No results found"
-        return render_template('search.html', message=message)
+        return render_template('whoops.html', message=message)
+
+
+def checkForMissingKeys(tracksList):
+    for result in tracksList:
+        if 'images' in result:
+            continue
+        else:
+            result['images'] = {'coverart': "https://cdn.pixabay.com/photo/2013/07/13/11/51/cd-158817_1280.png"}
+    return tracksList
